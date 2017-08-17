7ced021eae2d34273321bbf16456bbd8b697ae6a
MeatDuckXp <MeatDuckXp@hotmail.com>
2017-08-17 06:48:11 -0700

    Added Nunit test reference

---
+++ NamirialApiClient.Tests/NamirialServiceTests/NamirialServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using NamirialApiClient.Tests.TestBase;
+using NUnit.Framework;
+
+namespace NamirialApiClient.Tests.NamirialServiceTests
+{
+    [TestFixture]
+    public class NamirialServiceTest : NamirialClientTestBase
+    {
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ NamirialApiClient.Tests/TestBase/NamirialClientTestBase.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace NamirialApiClient.Tests.TestBase
+{
+    [TestFixture]
+    public abstract class NamirialClientTestBase 
+    {
+        [SetUp]
+        public virtual void SetUp()
+        { 
+            InitTest();
+        }
+
+        private void InitTest()
+        {
+            
+        }
+    }  
+}
